936aa9e2d63c40427d563e8254bca7cd8ac24c45
MattssonHubGit <erik.mattsson@live.se>
2019-05-23 22:02:15 +0200

    jumping behaviour, broken jump animations

--- KnightFight/Assets/Scripts/PlayerController.cs
+++ KnightFight/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float moveSpeed = 1f;
-    [HideInInspector] private bool canMove = true;
+    [SerializeField] private float moveSpeed = 400f;
+    [Space]
+    [SerializeField] private float jumpVelocity = 800f;
+    [SerializeField] private float fallMultiplier = 250f;
+    [SerializeField] private float lowJumpMultiplier = 200f;
 
+    /*[HideInInspector] */public bool isGrounded = true;
+    private bool canMove = true;
+    private float horizonatalMovement;
+    private float verticalMovement;
+
+
+    [Header("Components")]
+    private Rigidbody2D myRB;
     private Animator myAnim;
 
     void Start()
     {
-        myAnim = GetComponent<Animator>();
+        myAnim = this.GetComponent<Animator>();
+        myRB = this.GetComponent<Rigidbody2D>();
     }
-    
+
     void Update()
     {
         //FaceingController();
@@ -24,7 +36,6 @@ public class PlayerController : MonoBehaviour
 
     private void FaceingController()
     {
-
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (mousePos.x > transform.position.x)
         {
@@ -38,27 +49,52 @@ public class PlayerController : MonoBehaviour
 
     private void MoveController()
     {
-        //Get intput
         int moveX = 0;
-        int moveY = 0;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveX--;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveX++;
-        }
-
-
         if (canMove)
         {
-            //Execute input
-            transform.position += new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime;
+            //Get horizontal movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveX--;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveX++;
+            }
+            horizonatalMovement = moveX * moveSpeed * Time.deltaTime;
 
-            //Set animations
+            //Get vertical movement
+            //If grounded and pressing space, jump
+            if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                //isGrounded = false;
+                myRB.AddForce(Vector2.up * jumpVelocity * Time.deltaTime, ForceMode2D.Impulse);
+                myAnim.SetBool("IsJumping", true);
+            }
+
+            //Holding space jumps higher
+            if (myRB.velocity.y < 0)
+            {
+                myRB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
+            }
+            else if (myRB.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+            {
+                myRB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
+            }
+
+            //Put vertical and horizontal together
+            Vector2 newVel = Vector2.right * horizonatalMovement;
+            newVel.y = myRB.velocity.y;
+            myRB.velocity = newVel;
+        }
+
+
+        //Set animations and mirror
+        //Walking
+        if (isGrounded)
+        {
+            myAnim.SetBool("IsFalling", false);
+            myAnim.SetBool("IsJumping", false);
             if (moveX > 0)
             {
                 transform.localScale = new Vector3(3, 3, 3);
@@ -74,14 +110,35 @@ public class PlayerController : MonoBehaviour
                 myAnim.SetBool("IsMoving", false);
             }
         }
+        else //Airborn
+        {
+            if (myRB.velocity.x > 0)
+            {
+                transform.localScale = new Vector3(3, 3, 3);
+            }
+            else if (myRB.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-3, 3, 3);
+            }
 
+            if (myRB.velocity.y > 0)
+            {
+                myAnim.SetBool("IsFalling", true);
+                myAnim.SetBool("IsJumping", false);
+            }
+            else
+            {
+                myAnim.SetBool("IsFalling", false);
+                myAnim.SetBool("IsJumping", true);
+            }
+        }
 
     }
 
     private void AttackController()
     {
         //Attack on click
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && isGrounded)
         {
             canMove = false;
             myAnim.Play("player_attack_B");
@@ -90,7 +147,13 @@ public class PlayerController : MonoBehaviour
     }
 
     //Add on last keyframe of attack animations
-    public void AttackAnimationOver()
+    public void DisableMovement()
+    {
+        myRB.velocity = Vector3.zero;
+        canMove = false;
+    }
+
+    public void EnableMovement()
     {
         canMove = true;
     }
---
+++ KnightFight/Assets/Scripts/PlayerGroundChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundChecker : MonoBehaviour
+{
+    private PlayerController myPlayer;
+
+    private void Start()
+    {
+        myPlayer = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            myPlayer.isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            myPlayer.isGrounded = false;
+        }
+    }
+}
